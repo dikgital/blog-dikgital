3507a9d0bb003abc885ea9e234db5d51ae642a56
Diki Atmodjo <dikiatmodjo@gmail.com>
2025-08-11 08:29:14 +0800

    fix footer alignment

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -5,10 +5,10 @@ export function Footer() {
     <footer className="bg-neutral-50 border-t border-neutral-200 dark:bg-slate-800">
       <Container>
       <div className="flex flex-row justify-between">  
-        <div className="py-10 text-center text-sm text-gray-600 dark:text-gray-300">
+        <div className="py-10 text-left text-sm text-gray-600 dark:text-gray-300">
           <p>&copy; {new Date().getFullYear()} Diki Atmodjo. All rights reserved.</p>
         </div>
-        <div className="py-10 text-center text-sm text-gray-600 dark:text-gray-300">
+        <div className="py-10 text-left text-sm text-gray-600 dark:text-gray-300">
           <a
             href="https://www.linkedin.com/in/dikiatmodjo/"
             target="_blank"
